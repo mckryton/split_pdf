f76f693f54eff3a959b37abe70353bbbe6d28c9c
Matthias Carell <m_carell@yahoo.de>
2017-06-18 13:00:33 +0200

    assign file name after parameter validation

--- split_pdf.rb
+++ split_pdf.rb
@@ -2,12 +2,12 @@
 
 require 'combine_pdf'
 
-scanPdfPath = ARGV[0]
-
 if ARGV[0] == nil
   puts "usage: split_pdf.rb <pdf_file_name>"
   -1
 else
+  scanPdfPath = ARGV[0]
+
   #todo: validate path
 
   scanPdf = CombinePDF.load(scanPdfPath)
