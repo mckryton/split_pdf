e90875b3a13a895128bd99d3e147ab5919ad0f3e
Matthias Carell <rumpelcenter-github@yahoo.de>
2019-08-30 14:52:15 +0200

    add rotate (left) script

---
+++ rotate_pdf.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby
+
+require 'combine_pdf'
+
+if ::ARGV[0] == nil
+  puts "usage: rotate_pdf.rb <pdf_file_name>"
+  -1
+else
+  scanPdfPath = ARGV[0]
+
+  #todo: validate path
+
+  scanPdf = CombinePDF.load(scanPdfPath)
+  scanPdf.pages.each do |page|
+    page.rotate_left
+  end
+  scanPdf.save(scanPdfPath)
+end
